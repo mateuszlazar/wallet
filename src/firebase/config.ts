622cb7c03d59729dcc55e6964b5e0a2f789004fd
Mateusz Lazar <mateuszlazar@outlook.com>
2021-05-16 17:25:56 +0200

    feat: initial boilerplate

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  projectId: "YOUR PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET"
+};
